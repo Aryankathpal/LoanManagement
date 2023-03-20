e2527cbf047dbeca1a9dccfe46ee6a0ac981663d
aryan-kathpal kathpal <aryankathpal.cse19@chitkarauniversity.edu.in>
2023-03-20 10:54:26 +0530

    intital login

---
+++ src/app/Components/login/login.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { Route, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(private router : Router) { }
+
+  Signin(){
+    this.router.navigate(['/dashboard'])
+  }
+
+  ngOnInit(): void {
+  }
+
+}
