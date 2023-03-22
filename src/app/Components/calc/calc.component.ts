d1943e8ca107a671f0e27248cd68d8c7fd805038
AryanKathpal <aryankathpal.cse19@chitkarauniversity.edu.in>
2023-03-22 16:26:09 +0530

    calc 1.1

--- src/app/Components/calc/calc.component.ts
+++ src/app/Components/calc/calc.component.ts
@@ -7,11 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalcComponent {
   disabled = false;
-  max = 100;
-  min = 0;
   showTicks = true;
-  step = 1;
-  thumbLabel = false;
-  value = 10;
+  thumbLabel = false;     
+   amount = {
+    value : 10000,
+    min:10000,
+    max:1000000,
+    step:1000
+  }
+
+  roi = {
+    value : 1,
+    min:1,
+    max:30,
+    step:1
+  }
+
+  tenture = {
+    value : 1,
+    min:1,
+    max:30,
+    step:1
+  }
 
 }
